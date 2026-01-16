9fe44ef52760ee4863de1117470b22fb7bf61c23
rio-aman <amprajapati08893.com>
2026-01-16 13:59:27 +0530

    Smart Email AI Assistant
    •	Built an AI-powered email assistant to generate, summarize, and reply to emails intelligently.
    •	Developed backend using Spring Boot and frontend using React.
    •	Integrated OpenAI API for natural language processing and smart email generation.
    •	Created a browser extension to allow users to use the assistant directly inside their email client.
    •	Tech Stack: Spring Boot, React, OpenAI API, JavaScript, REST API

---
+++ email-writer-supporter/src/main/java/com/email/writer/EmailGeneratorService.java
@@ -0,0 +1,89 @@
+package com.email.writer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class EmailGeneratorService {
+
+    private final WebClient webClient;
+    private final String apiKey;
+
+    public EmailGeneratorService(
+            WebClient.Builder webClientBuilder,
+            @Value("${gemini.api.url}") String baseUrl,
+            @Value("${gemini.api.key}") String geminiApiKey) {
+
+        System.out.println("Loaded Gemini Base URL = " + baseUrl);  // DEBUG PRINT
+
+        this.webClient = webClientBuilder
+                .baseUrl(baseUrl)
+                .build();
+
+        this.apiKey = geminiApiKey;
+    }
+
+    public String generateEmailReply(EmailRequest emailRequest) {
+        // Build Prompt
+        String prompt = buildPrompt(emailRequest);
+
+        // Prepare raw json body
+        String requestBody = String.format("""
+                {
+                    "contents": [
+                      {
+                        "parts": [
+                          {
+                            "text": "%s"
+                          }
+                        ]
+                      }
+                    ]
+                  }""", prompt);
+
+        // Send Request
+        String response = webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1beta/models/gemini-2.5-flash:generateContent")
+                        .build())
+                .header("x-goog-api-key", apiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        // Extract Response
+        return extractResponseContent(response);
+    }
+
+    private String extractResponseContent(String response) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response);
+            return root.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email without subject.");
+        if (emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()) {
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("Original Email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
+}
